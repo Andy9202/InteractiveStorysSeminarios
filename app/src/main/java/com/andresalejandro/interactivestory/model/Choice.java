186326b298d71c8c4c9f470d75c4ac9990816608
Andy9202 <aatapia@udlanet.ec>
2018-01-19 23:31:34 -0500

    fuentes

---
+++ app/src/main/java/com/andresalejandro/interactivestory/model/Choice.java
@@ -0,0 +1,31 @@
+package com.andresalejandro.interactivestory.model;
+
+/**
+ * Created by AndresAlejandro on 1/18/2018.
+ */
+
+public class Choice {
+    private int textId;
+    private int nextPage;
+
+    public Choice(int textId, int nextPage) {
+        this.textId = textId;
+        this.nextPage = nextPage;
+    }
+
+    public int getTextId() {
+        return textId;
+    }
+
+    public void setTextId(int textId) {
+        this.textId = textId;
+    }
+
+    public int getNextPage() {
+        return nextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        this.nextPage = nextPage;
+    }
+}
